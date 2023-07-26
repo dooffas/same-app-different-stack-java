6a4b8395ac26a77be9dea7a18e9ab493eb6312df
dooffas <6917849+dooffas@users.noreply.github.com>
2023-07-26 10:46:40 +0000

    initial commit

---
+++ src/main/java/uk/co/gonova/sameappdifferentstackjava/SameAppDifferentStackJavaApplication.java
@@ -0,0 +1,13 @@
+package uk.co.gonova.sameappdifferentstackjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SameAppDifferentStackJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SameAppDifferentStackJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/co/gonova/sameappdifferentstackjava/SameAppDifferentStackJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.co.gonova.sameappdifferentstackjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SameAppDifferentStackJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
